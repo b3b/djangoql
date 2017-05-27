4a2bbfbbe18d11f2b85cf6cc30c8738bef13a2ff
b3b <ash.b3b@gmail.com>
2017-05-28 01:36:51 +0300

    admin: add dropdown with actions on saved queries

--- djangoql/static/djangoql/js/completion_admin.js
+++ djangoql/static/djangoql/js/completion_admin.js
@@ -1,5 +1,46 @@
-(function (DjangoQL) {
+(function (DjangoQL, showRelatedObjectPopup) {
   'use strict';
+
+  function saveQueryPopup(e) {
+    // Open Query object creation page, using Django admin popup functionality
+    e.preventDefault();
+    this.id = 'djangoql-save-query';
+    this.href = 'save-djangoql-query/?text=' +
+      encodeURIComponent(DjangoQL.textarea.value);
+    showRelatedObjectPopup(this);
+  }
+
+  function createDropdownMenu() {
+    var container = document.querySelector('.djangoql-save-icon-container');
+    var dropdown = document.createElement('div');
+
+    dropdown.className = 'djangoql-save-dropdown';
+    dropdown.appendChild(createDropdownItem(
+      'Show saved queries',
+      '\u2605', // star symbol as an icon
+      DjangoQL.switchSavedQueriesMode.bind(DjangoQL)
+    ));
+    dropdown.appendChild(createDropdownItem(
+      'Save query',
+      '\u2795', // bold Plus symbol as an icon
+      saveQueryPopup
+    ));
+    container.appendChild(dropdown);
+    return dropdown;
+  }
+
+  function createDropdownItem(label, icon, action) {
+    var item = document.createElement('a');
+    item.innerHTML = label;
+    item.className = 'djangoql-save-dropdown-item';
+    if (icon) {
+      item.setAttribute('data-content', icon);
+    }
+    if (action !== undefined) {
+      item.addEventListener('click', action);
+    }
+    return item;
+  };
 
   DjangoQL.DOMReady(function () {
     // Replace standard search input with textarea
@@ -21,8 +62,11 @@
     DjangoQL.init({
       introspections: 'introspect/',
       syntaxHelp: 'djangoql-syntax/',
+      savedQueriesIcon: true,
       selector: 'textarea[name=q]',
       autoResize: true
     });
+
+    createDropdownMenu();
   });
-}(window.DjangoQL));
+}(window.DjangoQL, window.showRelatedObjectPopup));
